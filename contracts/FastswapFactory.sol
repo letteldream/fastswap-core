60d17c12a64162d9f53db6c5bceefa7da00bed42
IntegralTeam <repo@sfxdx.ru>
2021-02-26 06:35:58 +0400

    Initial commit

---
+++ contracts/FastswapFactory.sol
@@ -0,0 +1,51 @@
+pragma solidity =0.5.16;
+
+import './interfaces/IFastswapFactory.sol';
+import './FastswapPair.sol';
+
+contract FastswapFactory is IFastswapFactory {
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(FastswapPair).creationCode));
+
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'Fastswap: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Fastswap: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Fastswap: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(FastswapPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IFastswapPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'Fastswap: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'Fastswap: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
